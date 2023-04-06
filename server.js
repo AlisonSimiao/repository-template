4f3af0f6b7fb51928023b70cb51d0ec09f9c45a1
Alison Simião <alison.s@multiplier.com.br>
2023-04-06 09:53:11 -0300

    fix: deletados

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./src/app')
-
-const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log('Server running on port: ' + port)
-  console.log('Started: ' + new Date().toLocaleString('pt-BR'))
-})
